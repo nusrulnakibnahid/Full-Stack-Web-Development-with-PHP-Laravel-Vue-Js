b7e9be676049cc7e82d9e8d415e55e5b1492f9a6
Your Name <nusrulnakibnahid.mail@gmail.com>
2024-11-30 22:36:49 +0600

    commit

---
+++ PHP/Variable/variable.php
@@ -0,0 +1,30 @@
+<?php
+$name = 'Bangladesh';
+$age = 52;
+$myname = 'Nahid';
+$word = 'age';
+$question = "How are you?";
+
+
+
+/* 
+Style Name:
+$myName = 'Nahid'; // CamelCase
+$MyName = "Nahid"; // TitleCase
+*/
+
+
+echo $age; 
+echo "\n"; 
+echo $$word;
+echo "\n"; 
+echo 'Hello '.$name;
+echo "\n"; 
+echo "Hello $name";
+echo "\n";
+echo "Hello $name, $question"; //or using this line: echo "Hello {$name}, {$question}";
+
+
+
+
+
