fb4ebd90f200aee3d5005a5f3b586883caaa32f9
Your Name <nusrulnakibnahid.mail@gmail.com>
2024-12-01 20:59:01 +0600

    commit

---
+++ PHP/Basic Arithmetic Operations/addition.php
@@ -0,0 +1,10 @@
+<?php
+$number = 5;
+
+//$number = $number + 12;
+//$number += 15;
+
+$number2 = $number + 30;
+
+echo $number, "\n", $number2;  //(\n) = new line 
+?>
---
+++ PHP/Basic Arithmetic Operations/subtraction.php
@@ -0,0 +1,8 @@
+<?php
+$number = 20;
+//$number = $number - 5;
+//$number -= 10;
+$number2 = $number - 2;
+
+echo $number,"\n", $number2;
+?>
