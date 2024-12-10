99251d988fb131e8c6ff748f01db5f1490a17afa
Your Name <nusrulnakibnahid.mail@gmail.com>
2024-12-10 23:22:13 +0600

    commit

--- Classes/Class - 05 (Function- Basic to Advanced)/function.php
+++ Classes/Class - 05 (Function- Basic to Advanced)/function.php
@@ -4,7 +4,7 @@ function  myName($name){
 
     echo "Hello $name";
 
-}
+};
 
 myName("Nahid");
 
@@ -14,7 +14,7 @@ echo "\n \n \n";
 function sumOfTwoNumbers($num1,$num2){
     $result = $num1 + $num2;
     return $result;
-}
+};
 
 $sumOfTest1 = sumOfTwoNumbers(25,45);
 $sumOfTest2 = sumOfTwoNumbers(10,20);
@@ -28,7 +28,7 @@ echo "\n \n \n";
 
 function greetings($names = "Sir!"){
     echo "Welcome $names";
-}
+};
 
 greetings("Nahid");
 
@@ -41,7 +41,7 @@ function sumOfNumbers(...$numbers){
     $sumResult = array_sum($numbers);
     return $sumResult;
 
-}
+};
 
 echo sumOfNumbers(10,30,56,67);
 
@@ -53,7 +53,7 @@ echo "\n \n \n";
 function sumOfTwoNumbersInt($num1,$num2):int{ //or using -> function sumOfTwoNumbersInt(int $num1,int $num2) or function sumOfTwoNumbersInt($num1,$num2): int | float
     $result = $num1 + $num2;
     return $result;
-}
+};
 
 echo sumOfTwoNumbersInt(20,30.77);
 
@@ -61,7 +61,117 @@ echo "\n \n \n";
 
 
 
+//Scope of variable ->
 
+//Local variable: 
+function variableScopeLocal(){
+    $nameLocal = "Nahid";
+
+    echo "Hello i'm $nameLocal";
+};
+variableScopeLocal();
+
+echo $nameLocal; //this line show error
+
+echo "\n";
+
+
+//Global variable:
+$staff = "Tonmoy";
+function variableScopeGlobal($staff){
+
+    global $staff;
+
+    echo "$staff please a make cup of coffee";
+
+
+};
+
+variableScopeGlobal($staff);
+
+echo "\n \n \n";
+
+
+
+
+//Anonymous function
+
+$anFunVar = function ($x, $y){
+
+    $z = $x * $y;
+    return $z;
+
+};
+
+echo $anFunVar(10,20);
+
+
+echo "\n \n \n";
+
+
+//Arrow function
+
+$arrowFnVar = fn($a, $b) => $a * $b;
+
+echo $arrowFnVar (10,5);
+
+echo "\n \n \n";
+
+
+
+
+//Recursive function
+
+function factWithRec($n){
+
+    if ($n == 0 ){
+        return 1;
+    }
+
+    $result = $n * factWithRec ($n-1);
+    return $result;
+
+}
+
+echo factWithRec(5);
+
+echo "\n \n \n";
+
+
+
+//Callback function
+function sayHello ($name){
+
+    return "Hello $name";
+
+}
+
+function greet($callback, $name){
+
+    return $callback($name);
+
+}
+
+echo greet("sayHello", "Nahid");
+
+
+echo "\n \n \n";
+
+
+//Reuseable function
+function mulTwoNumbers($s, $r){
+    return $s * $r;
+}
+function reuseableFun($a, $b, $c){
+
+    $sumResult = sumOfTwoNumbers($a, $b);
+    $mulResult = mulTwoNumbers($b, $c);
+    $finalResult = $sumResult + $mulResult;
+    return $finalResult;
+
+}
+
+echo reuseableFun(10, 20, 30);
 
 
 ?>
