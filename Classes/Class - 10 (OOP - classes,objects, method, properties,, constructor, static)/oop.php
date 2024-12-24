25cfc552118a07284ecbe4b7cb0095b00703beb4
Your Name <nusrulnakibnahid.mail@gmail.com>
2024-12-24 21:21:40 +0600

    commit

---
+++ Classes/Class - 10 (OOP - classes,objects, method, properties,, constructor, static)/oop.php
@@ -0,0 +1,89 @@
+<?php
+
+// Defining a class
+class Car {
+    // Defining properties (public, private, protected)
+    public $color = "red";
+    private $colorPrivate = "red";
+    protected $colorProtected = "red";
+}
+
+// Creating/Instantiating an object
+$car1 = new Car();
+echo $car1->color . "\n";
+
+// Constructor
+class CarWithConstructor {
+    public function __construct() {
+        echo "I am a Car constructor \n";
+    }
+}
+$myCar = new CarWithConstructor();
+
+// Passing parameters through constructor
+class CarWithParams {
+    public $color;
+
+    public function __construct($c) {
+        $this->color = $c;
+    }
+
+    public function drive() {
+        echo "I am driving the $this->color car \n";
+    }
+}
+
+$car3 = new CarWithParams("Red");
+echo $car3->color . "<br>";
+$car3->drive();
+
+$car4 = new CarWithParams("Blue");
+$car4->drive();
+
+// Calculator class example
+class Calculator {
+    public $num1;
+    public $num2;
+
+    public function __construct($num1, $num2) {
+        $this->num1 = $num1;
+        $this->num2 = $num2;
+    }
+
+    public function TowNumberSum() {
+        echo $this->num1 + $this->num2;
+    }
+
+    public function TowNumberSubstract() {
+        echo $this->num1 - $this->num2;
+    }
+}
+$calculator = new Calculator(5, 9);
+$calculator->TowNumberSum();
+
+// Static Properties
+class MyClass {
+    public static $myStaticProperty = "This is a static property";
+}
+
+echo MyClass::$myStaticProperty;
+
+// Static Methods
+class MyClassWithStaticMethod {
+    public static function myStaticMethod() {
+        echo "This is a static method";
+    }
+}
+MyClassWithStaticMethod::myStaticMethod();
+
+// Accessing Static Properties inside a class
+class MyClassWithStatic {
+    public static $value = "Static Value......";
+
+    public static function myStaticMethod() {
+        echo self::$value;
+    }
+}
+MyClassWithStatic::myStaticMethod();
+
+?>
---
+++ Classes/Class -11 (Abstract, Static, Final, Method Overloading, Method Overriding, Interface, Encapsulation, Polymorphism)/index.php
@@ -0,0 +1,21 @@
+<?php
+
+class Kitchen{
+
+    public $onion = 8;
+
+    static public function helloKitchen(){
+        echo "Hello I'm kitchen";
+    }
+
+}
+
+/*
+$obj = new Kitchen();
+echo $obj->onion;
+*/
+
+Kitchen:: helloKitchen();
+
+echo"\n \n \n";
+
