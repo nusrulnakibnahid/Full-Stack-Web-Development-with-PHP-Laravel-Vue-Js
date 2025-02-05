34d1dadc12fbb43caa497d58ee9f0aa1b92e8a95
Your Name <nusrulnakibnahid.mail@gmail.com>
2025-02-05 21:59:41 +0600

    style: add tasks.json for VSCode and ascii.php for character encoding examples

---
+++ PHP/Array & String/ascii.php
@@ -0,0 +1,12 @@
+ <?php
+ echo ord('A'); // Output: 65
+    
+ echo ord('a'); // Output: 97
+ 
+ echo PHP_EOL;
+ echo chr(65); // Output: A 
+   
+ echo chr(98); // Output: b
+    
+    echo chr(13);
+    // Output:
