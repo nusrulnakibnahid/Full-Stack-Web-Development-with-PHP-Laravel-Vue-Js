90e8e58d666086f55fbf226347bd0d59e1250615
Your Name <nusrulnakibnahid.mail@gmail.com>
2025-03-18 00:56:20 +0600

    feat: add initial database and view files for Laravel project

---
+++ Classes/Class - 35 (Laravel-Basics) [M-15, C-01]/ostad/storage/framework/views/67b238a31f7b927572a13d485822312e.php
@@ -0,0 +1,9 @@
+
+<h1><?php echo e($page); ?> - <?php echo e($p_number); ?></h1>
+
+<a href="<?php echo e(route('home')); ?>">Home</a>
+<a href="<?php echo e(route('home')); ?>">Home</a>
+<a href="<?php echo e(route('home')); ?>">Home</a>
+<a href="<?php echo e(route('home')); ?>">Home</a>
+
+<?php /**PATH C:\Users\USER\Desktop\Ostad Classes\PHP_BATCH6\M15_C1\ostad\resources\views/about.blade.php ENDPATH**/ ?>
